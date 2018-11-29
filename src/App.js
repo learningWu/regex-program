98d4bbdcce8e1ed3959af1a42db1baa736425d4d
learningWu <1600428416@qq.com>
2018-11-29 09:32:29 +0800

    增加正则界面

--- src/App.js
+++ src/App.js
@@ -1,28 +1,56 @@
-import React, { Component } from 'react';
+import React, {
+  Component
+} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      regexString: "",
+      regex: ""
+    }
+  }
+  regexStringChange = (e) => {
+    var value = e.target.value;
+    this.setState({
+      regexString: value
+    })
+  }
+  regexChange = (e) => {
+    var value = e.target.value;
+    this.setState({
+      regex: value
+    })
+  }
+  startMatch = (e) => {
+    let reg = new RegExp(this.state.regex);
+    // alert(reg.test(this.state.regexString) ? true : false);//test返回是否可以匹配到
+    alert(reg.exec(this.state.regexString));//exec返回匹配的结果
+
+  }
+
+  render() {
+    return (<
+      div className="App" >
+      <
+      header className="App-header" >
+        <
+      div className="App-row"> 待匹配字符串：<
+            input type="text" value={this.state.regexString} onChange={this.regexStringChange} /> < /div>
+             <
+      div className="App-row"> 请输入正则表达式：<
+              input type="text" value={this.state.regex} onChange={this.regexChange} /> < /div>
+    <div onClick={this.startMatch}>
+              测试匹配
+    </div>
+            <
+      /header> < /
+      div >
+                                                                                                                                );
+                                                                                                                              }
+                                                                                                                            }
+                                                                                                                            
 export default App;
