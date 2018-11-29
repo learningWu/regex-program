cef6611e7a03f9449df4c3f28d099c6e161e70ab
learningWu <1600428416@qq.com>
2018-11-29 17:33:27 +0800

    增加全局匹配ui

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
-import React, { Component } from "react";
+import React, {
+  Component
+} from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import checkbox from "antd/lib/checkbox";
+import "antd/lib/checkbox/style";
 
 class App extends Component {
   constructor(props) {
@@ -8,7 +12,8 @@ class App extends Component {
     this.state = {
       regexString: "",
       regex: "",
-      matchResult: ""
+      matchResult: "",
+      globalMatch: false
     };
   }
   regexStringChange = e => {
@@ -23,6 +28,22 @@ class App extends Component {
       regex: value
     });
   };
+  startGlobalMatch = e => {
+    let reg = new RegExp(this.state.regex);
+    // alert(reg.test(this.state.regexString) ? true : false);//test返回是否可以匹配到
+    //因为 this.props 和 this.state 可能是异步更新的，你不应该依靠它们的值来计算下一个状态。
+    this.setState((prevState, props) => {
+      let results = [];
+      for (const result of prevState.regexString.matchAll(reg)) {
+        results = [...results, result + "\n"];
+      };
+      // alert(result ? result : '没匹配到字符串'); //exec返回匹配的结果
+      return {
+        matchResult: results
+      };
+    });
+  };
+
   startMatch = e => {
     let reg = new RegExp(this.state.regex);
     // alert(reg.test(this.state.regexString) ? true : false);//test返回是否可以匹配到
@@ -34,37 +55,70 @@ class App extends Component {
         matchResult: result
       };
     });
-  };
+  }
 
+  checkChange = e => {
+    this.setState((prevState, props) => {
+      return {
+        globalMatch: !prevState.globalMatch
+      }
+    })
+  }
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <div className="App-row">
-            待匹配字符串：
-            <input
-              className="App-text"
-              type="text"
-              value={this.state.regexString}
-              onChange={this.regexStringChange}
-            />
-          </div>
-          <div className="App-input-regex-row">
-            请输入正则表达式：
-            <input
-              className="input"
-              type="text"
-              value={this.state.regex}
-              onChange={this.regexChange}
-            />
-            <button onClick={this.startMatch}>开始匹配 </button>
-          </div>
-          <div className="App-row">
-            匹配结果：
-            <text className="App-text"> {this.state.matchResult} </text>
-          </div>
-        </header>
-      </div>
+    return ( <
+      div className = "App" >
+      <
+      header className = "App-header" >
+      <
+      div className = "App-row" >
+      待匹配字符串： {
+        " "
+      } <
+      input className = "App-text"
+      type = "text"
+      value = {
+        this.state.regexString
+      }
+      onChange = {
+        this.regexStringChange
+      }
+      />{" "} < /
+      div > {
+        " "
+      } <
+      div className = "App-input-regex-row" >
+      请输入正则表达式： {
+        " "
+      } <
+      input className = "input"
+      type = "text"
+      value = {
+        this.state.regex
+      }
+      onChange = {
+        this.regexChange
+      }
+      />{" "} 全局匹配 <input type='checkbox' className='checkbox' defaultChecked={this.state.globalMatch} onChange={this.checkChange} ></input > {
+        " "
+      } <
+      button onClick = {
+        this.state.globalMatch ? this.startGlobalMatch : this.startMatch
+      } > 开始匹配 < /button>{" "} < /
+      div > {
+        " "
+      } <
+      div className = "App-row" >
+      匹配结果： {
+        " "
+      } <
+      text className = "App-text" > {
+        this.state.matchResult
+      } < /text>{" "} < /
+      div > {
+        " "
+      } <
+      /header>{" "} < /
+      div >
     );
   }
 }
